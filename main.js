61f9a3e985c4d3f83dbbb496f822a81e60ae34c0
spettigrew <sara.pettigrew5@gmail.com>
2020-02-25 14:38:44 -0700

    Many notes and types of typescript used

--- main.js
+++ main.js
@@ -2,3 +2,28 @@
 exports.__esModule = true;
 var message = "Welcome Back!";
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = "Codevolution";
+var isBeginner = true;
+var total = 0;
+var name = "Sara";
+var sentence = "My name is 4{name}\nI am a beginner in Typescript";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ["Tom", 25]; //must be in the same value order
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+; //first will start at the index value of 0 unless otherwise declared.
+var c = Color.Green;
+console.log(c);
--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
 export {}
 let message = "Welcome Back!";
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = "Codevolution";
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "Sara";
+
+let sentence: string = `My name is 4{name}
+I am a beginner in Typescript`;
+
+console.log(sentence)
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string, number] = ["Tom", 25]; //must be in the same value order
+
+enum Color {Red = 5, Green, Blue}; //first will start at the index value of 0 unless otherwise declared.
+
+let c: Color = Color.Green;
+console.log(c);
+
+
+
+
+
+
+
